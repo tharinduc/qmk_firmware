2642167570e79af20ce99d331dee1dce9b2af684
tharindu <chathurangewdt@gmail.com>
2024-11-26 21:55:31 +0100

    fixed syntax error

--- keyboards/my_lily58/keymaps/default/keymap.c
+++ keyboards/my_lily58/keymaps/default/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G, KC_LBRC,   KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                            KC_ESC, KC_LGUI, KC_LCTRL, KC_SPC,        KC_ENT, MO(_RAISE), KC_BSPC, KC_RCTRL
-   ,
+    ),
 
    /* RAISE
    * ,-----------------------------------------.                    ,-----------------------------------------.
